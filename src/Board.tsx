5afa287c1b6de1d2c7c9b1e3355024a1186bf3c1
Akbar Irwanda <irwandaa7@gmail.com>
2022-01-18 13:48:58 +0700

    [add] board and style

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { makepuzzle, solvepuzzle } from "sudoku";
+import "./board.scss";
+
+const puzzle = makepuzzle();
+const solved = solvepuzzle(puzzle);
+const pzlBoard: number[][] = [];
+while (puzzle.length) pzlBoard.push(puzzle.splice(0, 9));
 
 export default function Board() {
-  const puzzle = makepuzzle();
-  const solved = solvepuzzle(puzzle);
-  const pzlBoard: number[][] = [];
-  while (puzzle.length) pzlBoard.push(puzzle.splice(0, 9));
-
   return (
     <div className="tes">
       {pzlBoard.map((row, idx) => (
         <div className="row" key={idx}>
           {row.map((tile, idy) => (
-            <div className="tile" key={idy}></div>
+            <div
+              className={
+                "tile" +
+                (idx % 3 === 2 && idy % 3 === 2 && idx !== 8 && idy !== 8
+                  ? " border_hv"
+                  : idx % 3 === 2 && idx !== 8
+                  ? " border_h"
+                  : idy % 3 === 2 && idy !== 8
+                  ? " border_v"
+                  : "")
+              }
+              key={idy}
+            >
+              {tile ? tile + 1 : ""}
+            </div>
           ))}
         </div>
       ))}
