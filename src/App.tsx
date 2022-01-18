9c544c9b759cccdff1b31b5945b35547d2296dbd
Akbar Irwanda <irwandaa7@gmail.com>
2022-01-18 18:06:39 +0700

    [add] board style

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import React from "react";
 import "./App.css";
 import Board from "./Board";
 
+export enum theme {
+  dark,
+  light,
+}
+export enum difficulty {
+  beginner,
+  easy,
+  medium,
+  hard,
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Sudoku</h1>
       </header>
-      <Board />
+      <Board difficulty={difficulty.hard} theme={theme.dark} />
     </div>
   );
 }
--- src/Board.tsx
+++ src/Board.tsx
@@ -1,21 +1,53 @@
 import React from "react";
 import { makepuzzle, solvepuzzle } from "sudoku";
 import "./board.scss";
+import { difficulty, theme } from "./App";
+
+interface gameSetting {
+  difficulty: difficulty;
+  theme: theme;
+}
 
 const puzzle = makepuzzle();
 const solved = solvepuzzle(puzzle);
 const pzlBoard: number[][] = [];
-while (puzzle.length) pzlBoard.push(puzzle.splice(0, 9));
+for (let i = 0; i < solved.length; i += 9) {
+  pzlBoard.push(solved.slice(i, i + 9));
+}
+const hidden: boolean[][] = [];
 
-export default function Board() {
+// Beginner = 26-30
+// Easy = 36-40
+// Medium = 46-50
+// Hard = 56-60
+function setHidden(dif: number) {
+  let num: number = Math.floor(Math.random() * 5) + 26 + dif * 10;
+  for (let i = 0; i < 9; i++) {
+    hidden[i] = Array(9).fill(true);
+  }
+
+  while (num > 0) {
+    let idx = Math.floor(Math.random() * 9);
+    let idy = Math.floor(Math.random() * 9);
+    if (hidden[idx][idy]) {
+      hidden[idx][idy] = false;
+      num--;
+    }
+  }
+}
+
+export default function Board(prop: gameSetting) {
+  setHidden(prop.difficulty);
+
   return (
-    <div className="tes">
+    <div className="game">
       {pzlBoard.map((row, idx) => (
         <div className="row" key={idx}>
           {row.map((tile, idy) => (
             <div
               className={
                 "tile" +
+                (hidden[idx][idy] ? " disable" : "") +
                 (idx % 3 === 2 && idy % 3 === 2 && idx !== 8 && idy !== 8
                   ? " border_hv"
                   : idx % 3 === 2 && idx !== 8
@@ -26,7 +58,7 @@ export default function Board() {
               }
               key={idy}
             >
-              {tile ? tile + 1 : ""}
+              {hidden[idx][idy] ? tile + 1 : ""}
             </div>
           ))}
         </div>
